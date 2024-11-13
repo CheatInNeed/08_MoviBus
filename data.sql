f29dd1088a77eabdfd484efbae4223175230c987
Alexander Lillerup-rasmussen <alex@alexanders-air.students.clients.local>
2024-11-13 13:51:13 +0100

    added more busride

--- data.sql
+++ data.sql
@@ -7,8 +7,8 @@ INSERT INTO BusStop (StopName, StopGPS) VALUES
 ('Darnassus', '48.85661,2.35222'),
 ('Thunder Bluff', '35.6895,139.69171');
 
--- Passanger Data
-INSERT INTO Passanger (ID, Email, FirstName, LastName) VALUES
+-- Passenger Data
+INSERT INTO Passenger (ID, Email, FirstName, LastName) VALUES
 (1, 'thrall@horde.com', 'Thrall', 'Doomhammer'),
 (2, 'jaina@alliance.com', 'Jaina', 'Proudmoore'),
 (3, 'sylvanas@horde.com', 'Sylvanas', 'Windrunner'),
@@ -25,12 +25,27 @@ INSERT INTO BusLine (LineName, FinalDestination) VALUES
 
 -- BusRide Data
 INSERT INTO BusRide (RideDate, StartTime, Duration, StartGPS, EndGPS, Passanger_ID, Line_FinalDestination, Line_Name) VALUES
-('2024-11-01', '08:00:00', '02:00:00', '51.50853,-0.12574', '34.05223,-118.24368', 1, 'Orgrimmar', 'Horde Express'),
-('2024-11-02', '09:00:00', '01:30:00', '40.71278,-74.00594', '51.50853,-0.12574', 2, 'Stormwind', 'Alliance Express'),
-('2024-11-03', '10:00:00', '01:45:00', '34.05223,-118.24368', '35.6895,139.69171', 3, 'Thunder Bluff', 'Great Lift'),
-('2024-11-04', '11:00:00', '02:15:00', '48.85661,2.35222', '40.71278,-74.00594', 4, 'Ironforge', 'Mountain Pass'),
-('2024-11-05', '12:00:00', '01:00:00', '35.6895,139.69171', '48.85661,2.35222', 5, 'Darnassus', 'Elven Path');
-
+('2024-11-01', '08:00:00', '02:00:00', '51.50', '34.05', 1, 'Orgrimmar', 'Horde Express'),
+('2024-11-01', '10:30:00', '01:30:00', '34.05', '51.50', 1, 'Stormwind', 'Alliance Express'),
+('2024-11-02', '09:00:00', '01:30:00', '40.71', '51.50', 2, 'Stormwind', 'Alliance Express'),
+('2024-11-02', '11:00:00', '02:00:00', '51.50', '40.71', 2, 'Ironforge', 'Mountain Pass'),
+('2024-11-03', '10:00:00', '01:45:00', '34.05', '35.68', 3, 'Thunder Bluff', 'Great Lift'),
+('2024-11-03', '12:00:00', '01:45:00', '35.68', '34.05', 3, 'Orgrimmar', 'Horde Express'),
+('2024-11-04', '11:00:00', '02:15:00', '48.85', '40.71', 4, 'Ironforge', 'Mountain Pass'),
+('2024-11-04', '14:00:00', '02:15:00', '40.71', '48.85', 4, 'Darnassus', 'Elven Path'),
+('2024-11-05', '12:00:00', '01:00:00', '35.68', '48.85', 5, 'Darnassus', 'Elven Path'),
+('2024-11-05', '13:30:00', '01:00:00', '48.85', '35.68', 5, 'Thunder Bluff', 'Great Lift'),
+('2024-11-06', '09:30:00', '01:30:00', '51.50', '34.05', 1, 'Orgrimmar', 'Horde Express'),
+('2024-11-06', '11:00:00', '01:30:00', '34.05', '51.50', 1, 'Stormwind', 'Alliance Express'),
+('2024-11-07', '08:00:00', '01:45:00', '40.71', '48.85', 2, 'Darnassus', 'Elven Path'),
+('2024-11-07', '10:00:00', '01:45:00', '48.85', '40.71', 2, 'Ironforge', 'Mountain Pass'),
+('2024-11-08', '09:00:00', '02:00:00', '34.05', '35.68', 3, 'Thunder Bluff', 'Great Lift'),
+('2024-11-08', '11:30:00', '02:00:00', '35.68', '34.05', 3, 'Orgrimmar', 'Horde Express'),
+('2024-11-09', '10:00:00', '02:15:00', '48.85', '40.71', 4, 'Ironforge', 'Mountain Pass'),
+('2024-11-09', '13:00:00', '02:15:00', '40.71', '48.85', 4, 'Darnassus', 'Elven Path'),
+('2024-11-10', '11:00:00', '01:00:00', '35.68', '48.85', 5, 'Darnassus', 'Elven Path'),
+('2024-11-10', '12:30:00', '01:00:00', '48.85', '35.68', 5, 'Thunder Bluff', 'Great Lift'),
+('2024-11-11', '08:00:00', '01:30:00', '51.50', '34.05', 1, 'Orgrimmar', 'Horde Express'),
 -- Serves Data
 INSERT INTO Serves (StopNr, Stop_GPS, Line_FinalDestination, Line_Name) VALUES
 (1, '51.50853,-0.12574', 'Stormwind', 'Alliance Express'),
@@ -40,7 +55,7 @@ INSERT INTO Serves (StopNr, Stop_GPS, Line_FinalDestination, Line_Name) VALUES
 (5, '35.6895,139.69171', 'Thunder Bluff', 'Great Lift');
 
 -- Address Data
-INSERT INTO Address (Passanger_ID, ZIP, StreeName, CivicNumber, City, Country) VALUES
+INSERT INTO Address (Passenger_ID, ZIP, StreeName, CivicNumber, City, Country) VALUES
 (1, 1234, 'Grommash Hold', '1', 'Orgrimmar', 'Durotar'),
 (2, 5678, 'Mage Quarter', '2', 'Stormwind', 'Elwynn Forest'),
 (3, 9101, 'Royal District', '3', 'Undercity', 'Tirisfal Glades'),
@@ -48,7 +63,7 @@ INSERT INTO Address (Passanger_ID, ZIP, StreeName, CivicNumber, City, Country) V
 (5, 3141, 'Elder Rise', '5', 'Thunder Bluff', 'Mulgore');
 
 -- PhoneNr Data
-INSERT INTO PhoneNr (Passanger_ID, PhoneNr) VALUES
+INSERT INTO PhoneNr (Passenger_ID, PhoneNr) VALUES
 (1, '+1-555-THRALL'),
 (2, '+1-555-JAINA'),
 (3, '+1-555-SYLVANAS'),
